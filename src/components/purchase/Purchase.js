4c6cd89cc54e5e7784b4c56280b5f54cfcb2cc51
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2022-03-10 18:09:58 +0100

    [PICTURES]

--- src/components/purchase/Purchase.js
+++ src/components/purchase/Purchase.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {Button, TextField, ThemeProvider} from "@mui/material";
-import { createTheme } from '@mui/material/styles';
+import {createTheme} from '@mui/material/styles';
+import scooter1 from '../../images/scooter-1.jpg';
+import scooter2 from '../../images/scooter-4.jpg';
+import './Purchase.css'
 
 const theme = createTheme({
     status: {
@@ -19,19 +22,19 @@ const theme = createTheme({
 
 export const Purchase = () => {
     return <div>
-        <form>
+        <img src={scooter1} className={"picture"} alt=""/>
+        <form className={"form"}>
             <br/><br/>
             <TextField id="outlined-basic" label="Nom" variant="outlined" required/>
             <br/><br/>
             <TextField id="outlined-basic" label="Prénom" variant="outlined" required/>
             <br/><br/>
-            <TextField id="outlined-basic" label="Email" variant="outlined" type={"email"}
-                       errorText={"Veuillez rentrer un email valide"}
-                       required/>
+            <TextField id="outlined-basic" label="Email" variant="outlined" type={"email"} required/>
             <br/><br/>
             <ThemeProvider theme={theme}>
                 <Button variant="contained" color="neutral" type={"submit"}>J'achète mon scooter</Button>
             </ThemeProvider>
         </form>
+        <img src={scooter2} className={"picture"} alt=""/>
     </div>
 }
