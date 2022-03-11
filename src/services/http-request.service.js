b53fac78e0e99f3ac9b798644155700a1ba77074
Adem Mrizak <adem.mrizak@gmail.com>
2022-03-11 10:28:10 +0100

    Dotenv implementation

--- src/services/http-request.service.js
+++ src/services/http-request.service.js
@@ -1,5 +1,3 @@
-export const API_URL = 'http://localhost:3000/';
-
 export const sendGetRequest = async (url) => {
     return fetch(url);
 };
--- src/services/purchase.service.js
+++ src/services/purchase.service.js
@@ -1,5 +1,5 @@
-import {API_URL, sendPostRequest} from "./http-request.service";
+import {sendPostRequest} from "./http-request.service";
 
 export const sendPurchase = async (firstname, lastname, email) => {
-    return (await sendPostRequest(API_URL + "buyers", {firstname, lastname, email})).body;
+    return (await sendPostRequest(process.env.API_URL + "buyers", {firstname, lastname, email})).body;
 };
